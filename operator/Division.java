5ff5661852aebe3a3e1afde47cb5f28640220b95
si eun <tldms123455@naver.com>
2024-01-15 16:38:14 +0900

    implements division

---
+++ operator/Division.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class Division implements ArithmeticOperator {
+    @Override
+    public boolean support(String operator) {
+
+        return operator.equals("/");
+    }
+
+    @Override
+    public boolean calc(int x, int y) {
+
+        return x / y;
+    }
+}
